1a9b43c3ae2ef8ab31c512a1a023d3a34ae645ef
Nathan <127557198+ArchiesMaster@users.noreply.github.com>
2024-10-14 20:30:57 +1000

    Update app.py

--- app.py
+++ app.py
@@ -74,4 +74,4 @@ def recommend():
     return render_template('index.html', recommendations=recommendations, genre=genre)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
