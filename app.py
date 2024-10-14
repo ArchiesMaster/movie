d56eeed74681d1b2ece0db22244c7b9074f066fa
Nathan <127557198+ArchiesMaster@users.noreply.github.com>
2024-10-14 20:20:33 +1000

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import pandas as pd
 import random
 from flask import Flask, render_template, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 def load_movies(file_path):
     """Load the MovieLens dataset."""
